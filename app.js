aacae270dc03a9a4d5422df9cc5e02c9a315cae4
Kevin Liao <me@liaokev.in>
2015-07-18 15:26:53 +0800

    Current state of Skeleton Project

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 (function() {
     var app = angular.module('kevinliao_website', [ ]);
 
-    app.controller('
+    app.controller('home_page', function() {
+        this.homePageData = home_page_info;
+        console.log(this.homePageData.greeting);
+        console.log(this.homePageData.profile_pic);
+    });
+
+    var home_page_info = {
+        title: "Kevin Liao", 
+        greeting: "Hello. I'm Kevin. I'm a full stack developer based in Taipei, Taiwan and Haverford, Pennsylvania. Wherever I am, I'm ready to build something awesome with you. \n Are you interested? Let's work together.  ",
+        profile_pic: 'images/profile.jpg'
+    };
+
+    var portfolio = [
+        {
+            name: "Polititrends",
+            description: "A web app that allows you to research how the New York Times' political leanings have changed on an issue over time.",
+            technologies: "HTML, CSS, JavaScript, Python, indico.io, Google App Engine",
+        }, 
+    ];
+})();
